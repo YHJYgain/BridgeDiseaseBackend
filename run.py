c5789912b17c631bc19d834f24fe12d53d1bd004
Re.Gin <1583952973@qq.com>
2025-03-14 16:03:31 +0800

    chore: 修改系统名称

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     """
     启动 Flask 应用。当脚本直接执行时，运行 Flask 应用的开发服务器。
     """
-    app.logger.info('桥梁病害系统后端服务开始运行。')
+    app.logger.info('桥梁病害检测与分割系统后端服务开始运行……')
     app.run()
